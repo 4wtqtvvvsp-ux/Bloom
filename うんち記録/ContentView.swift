f9baeab193b49c661120a6a71875b4c0975ebb7e
a <>
2026-02-27 09:31:51 +0900

    Initial Commit

---
+++ うんち記録/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  うんち記録
+//
+//  Created by Apple on 2026/02/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
